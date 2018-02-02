8ec7004b720bb15d94ea6a293464345a28713081
wictor <wictorsmartins@gmail.com>
2018-02-02 10:46:55 -0200

    sec commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn version2.wsgi --log-file -
--- version2/settings.py
+++ version2/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
+import dj_database_url
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -89,6 +92,13 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+django_heroku.settings(locals())
+
+DATABASES['default'] = dj_database_url.config()
+DATABASES['default']['CONN_MAX_AGE'] = 500
+DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
+
 
 
 # Password validation
